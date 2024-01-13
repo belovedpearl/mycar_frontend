f9862579590c9c76eede77bf693c58969ea19a97
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-13 21:01:50 +0000

    Modify signup form styles and handle change e to input field

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
+import CoverImage from '../../assets/img.jpeg';
+
 import { Button, Form, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState (
+    {
+      username: '',
+      password1: '',
+      password2: '',
+    }
+  )
+  const {username, password1, password2} = signUpData
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  
+
   return (
     <Row className={styles.Row}>
+      {/* Left side */}
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className= "p-4">
           <h1 className={styles.Header}>SIGN UP</h1>
@@ -22,7 +43,9 @@ const SignUpForm = () => {
                 className={styles.Input}
                 type="text" 
                 placeholder="Enter username" 
-                name="username" 
+                name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
 
@@ -33,6 +56,8 @@ const SignUpForm = () => {
                 type="password" 
                 placeholder="Password"
                 name="password1"
+                value={password1}
+                onChange={handleChange}
               />
             </Form.Group>
 
@@ -43,6 +68,8 @@ const SignUpForm = () => {
                 type="password" 
                 placeholder="Confirm Password"
                 name="password2" 
+                value={password2}
+                onChange={handleChange}
               />
             </Form.Group>
             <Button 
@@ -56,19 +83,24 @@ const SignUpForm = () => {
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <p className={styles.LinkText}>
-            <span className="mr-2">Already have an account? </span><Link className={styles.Link} to="/signin">Sign in</Link>
+            <span className="mr-2">
+              Already have an account?
+            </span>
+              <Link className={styles.Link} to="/signin">
+                Sign in
+              </Link>
           </p>
         </Container>
       </Col>
+      {/* Right side */}
       <Col
         md={6}
         className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
       >
         <Image
-          className={`${appStyles.FillerImage}`}
-          src={
-            "https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
-          }
+          className={`${appStyles.CoverImage} img-fluid rounded-circle`}
+          src= {CoverImage}
+          alt="Sign up page cover picture"
         />
       </Col>
     </Row>

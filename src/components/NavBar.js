2ed6cb8381ee365c3d209a6f6f45c861893f3bab
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-13 13:10:23 +0000

    Add styles and logo to the NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,31 @@
 import React from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
+import styles from '../styles/NavBar.module.css'
+import logo from '../assets/logo.png'
 
 const NavBar = () => {
   return (
     <div>
-        <Navbar bg="light" expand="md" fixed='top'>
+        <Navbar className={styles.NavBar} expand="md" fixed='top'>
             <Container>
-                <Navbar.Brand >Logo</Navbar.Brand>
+                <Navbar.Brand>
+                    <img src={logo} alt='Brand logo' height='40' />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link>
-                            <i className='fas fa-home mr-2'> </i>
-                            Home  
+                        <Nav.Link className={`${styles.NavLink} mr-2`}>
+                            <i className='fas fa-home mr-1'></i>
+                            Home
                         </Nav.Link>
-                        <Nav.Link>
-                            <i className="fas fa-right-to-bracket mr-2"></i>
+                        <Nav.Link className={`${styles.NavLink} mr-2`}>
+                            <i className="fas fa-right-to-bracket mr-1"></i>
                             Sign in
                         </Nav.Link>
-                        <Nav.Link>
-                            <i className="fas fa-user-plus mr-2"></i>
+                        <Nav.Link className={`${styles.NavLink} mr-2`}>
+                            <i className="fas fa-user-plus mr-1"></i>
                             Sign up
-                        </Nav.Link>
-                        
-                    
+                        </Nav.Link>                   
                     </Nav>
                 </Navbar.Collapse>
             </Container>

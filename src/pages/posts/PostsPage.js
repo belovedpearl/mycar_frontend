46d21da49c4081f63dc43b45172a081af7c529cd
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-18 12:43:16 +0000

    Add infinite scroll to posts

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -12,6 +12,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import NoResults from '../../assets/no-results.png'
 import Asset from '../../components/Assets'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 
 function PostsPage({message, filter= ''}) {
@@ -62,9 +63,14 @@ function PostsPage({message, filter= ''}) {
         {hasLoaded ? (
           <>
           {posts.results.length ? (
-            posts.results.map((post) => (
+            <InfiniteScroll children= {posts.results.map((post) => (
               <Post key={post.id} {...post} setPosts={setPosts} />
-            ))
+              ))}
+              dataLength={posts.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!posts.next}
+              next={() => fetchMoreData(posts, setPosts)}
+            />
           ) : (
             <Container className={appStyles.NoResults}>
                 <Asset src={NoResults} message={message} />

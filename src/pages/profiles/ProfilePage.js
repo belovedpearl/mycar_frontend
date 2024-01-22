01c8a2aa6ceb0eff953a5e14266d30659732b561
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-22 15:27:54 +0000

    Add user's upvoted and downvoted posts to profile page

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -22,10 +22,6 @@ import Post from '../posts/Post'
 import InfiniteScroll from "react-infinite-scroll-component";
 import Tab from "react-bootstrap/Tab";
 
-
-
-
-
 function ProfilePage() { 
     const [hasLoaded, setHasLoaded] = useState(false);
     const currentUser = useCurrentUser();
@@ -35,15 +31,23 @@ function ProfilePage() {
     const [profile] = pageProfile.results;
     const is_owner = currentUser?.username === profile?.owner;
     const [profilePosts, setProfilePosts] = useState({ results: [] });
+    const [upvotePosts, setUpvotePosts] = useState({ results: [] });
+    const [downvotePosts, setDownvotePosts] = useState({ results: [] });
      
     useEffect(() => {
         const fetchData = async() => {
             try{
-                const [{data: pageProfile}, { data: profilePosts }] = await Promise.all(
+                const [
+                    {data: pageProfile}, 
+                    { data: profilePosts }, 
+                    {data: upvotePosts}, 
+                    {data: downvotePosts}
+                        ] = await Promise.all(
                     [
                         axiosReq.get(`/profiles/${id}/`),
                         axiosReq.get(`/posts/?owner__profile=${id}`),
-                        
+                        axiosReq.get(`/posts/?upvotes__owner__profile=${id}&ordering=-upvotes__created_at&`),
+                        axiosReq.get(`/posts/?downvotes__owner__profile=${id}&ordering=-downvotes__created_at&`),                       
                     ]
                 )
                 setProfileData(prevState => ({
@@ -51,13 +55,15 @@ function ProfilePage() {
                     pageProfile: {results: [pageProfile]}
                 }))
                 setProfilePosts(profilePosts);
+                setUpvotePosts(upvotePosts)
+                setDownvotePosts(downvotePosts)
                 setHasLoaded(true);
             }catch(err){
                 console.log(err)
             }
         }
         fetchData(); 
-    }, [id, setProfileData])
+    }, [id, upvotePosts, downvotePosts, setProfileData])
 
     const mainProfile = (
         <>
@@ -79,15 +85,24 @@ function ProfilePage() {
                     {profile?.location && <p className="m-2">
                         <FaLocationDot />
                         {profile.location}
-                    </p>}
+                        </p>
+                    }
                     <Row className="justify-content-center no-gutters">
                         <Col xs={4} className="my-2">
-                            <div>{profile?.posts_count}</div>
-                            <div>Posts</div>
+                            <div>
+                                {profile?.posts_count}
+                            </div>
+                            <div>
+                                Posts
+                            </div>
                         </Col>
                         <Col xs={4} className="my-2">
-                            <div>{profile?.followers_count}</div>
-                            <div >Followers</div>
+                            <div>
+                                {profile?.followers_count}
+                            </div>
+                            <div>
+                                Followers
+                            </div>
                         </Col>
                         <Col xs={4} className="my-2">
                             <div>
@@ -144,22 +159,60 @@ function ProfilePage() {
                     />
             )}
         </>
-  );
+    );
     const upvotes = (
-    'Nothing here now'
+        <>
+            {upvotePosts.results.length ? (
+                <InfiniteScroll
+                    children={upvotePosts.results.map((post) => (
+                        <Post key={post.id} {...post} setPosts={setUpvotePosts} />
+                    ))}
+                    dataLength={upvotePosts.results.length}
+                    loader={ <Asset spinner />}
+                    hasMore={ !!upvotePosts.next }
+                    next={() => fetchMoreData(upvotePosts, setUpvotePosts)}
+                />
+                ) : (
+                    <Asset
+                        src={NoResults}
+                        message={`No results found ${
+                            profile?.owner
+                        } has not upvoted any post.`}
+                    />
+            )}
+        </>
     )
     const downvotes = (
-        'Nothing here now'
-        )
+        <>
+            {downvotePosts.results.length ? (
+                <InfiniteScroll
+                    children={downvotePosts.results.map((post) => (
+                        <Post key={post.id} {...post} setPosts={setDownvotePosts} />
+                    ))}
+                    dataLength={downvotePosts.results.length}
+                    loader={ <Asset spinner />}
+                    hasMore={ !!downvotePosts.next }
+                    next={() => fetchMoreData(downvotePosts, setDownvotePosts)}
+                />
+                ) : (
+                    <Asset
+                        src={NoResults}
+                        message={`No results found ${
+                            profile?.owner
+                        } has not downvoted any post.`}
+                    />
+            )}
+        </>
+    )
 
   const profileSections = (
         <>
             <Tabs defaultActiveKey="posts" id="profile-tab">
                 <Tab eventKey="posts" title={`${profile?.owner}'s Posts`}>
-                    {mainProfilePosts}
+                    { mainProfilePosts }
                 </Tab>
                 <Tab eventKey="upvotes" title={`${profile?.owner}'s Upvotes`}>
-                    { upvotes}
+                    { upvotes }
                 </Tab>
                 <Tab eventKey="downvotes" title={`${profile?.owner}'s Downvotes`}>
                     { downvotes }
@@ -172,23 +225,24 @@ function ProfilePage() {
     <Row>
         <Col lg={2} className="d-none d-lg-block p-0 p-lg-2">
             <p>popular profiles for desktop</p>
-          </Col>
-      <Col className="py-2 p-0 p-lg-2" lg={6}>
-        <PopularProfiles mobile />
-        <Container className={appStyles.Content}>
-          {hasLoaded ? (
-            <>
-              {mainProfile}
-              {profileSections}
-            </>
-          ) : (
-            <Asset spinner />
-          )}
-        </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        <PopularProfiles />
-      </Col>
+        </Col>
+        <Col className="py-2 p-0 p-lg-2" lg={6}>
+            <PopularProfiles mobile />
+            <Container className={ appStyles.Content }>
+                {hasLoaded ? (
+                    <>
+                        {mainProfile}
+                        {profileSections}
+                    </>
+                    ) : (
+                        <Asset spinner />
+                    )
+                }
+            </Container>
+        </Col>
+        <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+            <PopularProfiles />
+        </Col>
     </Row>
   );
  }

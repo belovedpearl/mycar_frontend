16857de1e0b81e5a5b32935532c9753a6b315cbb
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-25 11:09:48 +0000

    Setup test and add test to check for NavBar rendering required links

---
+++ src/components/__tests__/NavBar.test.js
@@ -0,0 +1,17 @@
+import { render, screen} from "@testing-library/react"
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "../NavBar";
+
+
+test('renders NavBar', () => {
+   render(<Router>
+    <NavBar />
+   </Router>
+   )
+  const signInLink = screen.getByRole("link", { name: "Sign in" });
+  expect(signInLink).toBeInTheDocument();
+  const signUpLink = screen.getByRole("link", { name: "Sign up" });
+  expect(signUpLink).toBeInTheDocument();
+  const HomeLink = screen.getByRole("link", { name: "Home" });
+  expect(HomeLink).toBeInTheDocument();
+})
---
+++ src/mocks/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from "msw"
+
+const baseURL = "https://mycardrfapi-d64556077ed4.herokuapp.com/"
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+      return res(
+        ctx.json({
+            "pk": 1,
+            "username": OlaIfe,
+            "email": "",
+            "first_name": "",
+            "last_name": "",
+            "profile_id": 1,
+            "profile_image": "https://res.cloudinary.com/dwhb1z4jd/image/upload/v1/media/../default_profile_vpfbb8"
+          })
+      );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+      return res(ctx.status(200));
+    }),
+  ];
